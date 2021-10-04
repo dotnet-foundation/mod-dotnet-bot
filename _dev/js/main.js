a0b3b9602703ec8d60211a59a83c6fdc532dbc8d
Ihor Sychevskyi <arhell333@gmail.com>
2021-10-05 00:54:44 +0300

    reformat js (#17)

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -7,11 +7,11 @@ const Controls = require('./modules/controls.js');
 const Objects = require('./modules/objects.js');
 const Save = require('./modules/save.js');
 
-$(function(){
+$(function () {
 //create the app.
-let app = new App();
-let viewport = new Viewport();
-let controls = new Controls();
-let objects = new Objects();
-let save = new Save();
+  let app = new App();
+  let viewport = new Viewport();
+  let controls = new Controls();
+  let objects = new Objects();
+  let save = new Save();
 });

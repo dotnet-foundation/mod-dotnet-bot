344ef78c6c7f8f44eed8f7d6db54f7841cc825c1
Arhell <arhell333@gmail.com>
2022-07-20 16:04:07 +0300

    reformat js

--- _dev/js/modules/app.js
+++ _dev/js/modules/app.js
@@ -4,57 +4,57 @@ const $ = require('jquery');
 
 class App {
 
-  constructor(options) {
+    constructor(options) {
 
 
-    if (!('ontouchstart' in window)) {
-      document.documentElement.classList.add('no-touch');
+        if (!('ontouchstart' in window)) {
+            document.documentElement.classList.add('no-touch');
+        }
+
+        if ('ontouchstart' in window) {
+            document.documentElement.classList.add('is-touch');
+        }
+
+        if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+            if (navigator.appVersion.indexOf('Trident') === -1) {
+                document.documentElement.classList.add('isEDGE');
+            } else {
+                $('html').addClass('isIE isIE11');
+            }
+        }
+
+        var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) {
+            return p.toString() === "[object SafariRemoteNotification]";
+        })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+        if (isSafari) {
+            document.body.classList.add('browser-safari');
+        }
+
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.body.classList.add('darkmode');
+        }
+
+        if (window.location.hostname == 'localhost' | window.location.hostname == '127.0.0.1') {
+            document.body.classList.add('localhost');
+        }
+
+        $(document).ready(function () {
+            $('body').addClass('page-loaded');
+
+            const toggleMenu = $('.header-menu-icon')
+            const menuList = $('.header-menu ul')
+
+            $(toggleMenu).click(function () {
+                $(menuList).slideToggle()
+            })
+
+        });
+
+        setTimeout(function () {
+            $('body').addClass('page-loaded');
+        }, 5000);
     }
-
-    if ('ontouchstart' in window) {
-      document.documentElement.classList.add('is-touch');
-    }
-
-    if (document.documentMode || /Edge/.test(navigator.userAgent)) {
-      if (navigator.appVersion.indexOf('Trident') === -1) {
-        document.documentElement.classList.add('isEDGE');
-      } else {
-        $('html').addClass('isIE isIE11');
-      }
-    }
-
-    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) {
-      return p.toString() === "[object SafariRemoteNotification]";
-    })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-
-    if (isSafari) {
-      document.body.classList.add('browser-safari');
-    }
-
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.body.classList.add('darkmode');
-    }
-
-    if (window.location.hostname == 'localhost' | window.location.hostname == '127.0.0.1') {
-      document.body.classList.add('localhost');
-    }
-
-    $(document).ready(function () {
-      $('body').addClass('page-loaded');
-
-      const toggleMenu = $('.header-menu-icon')
-      const menuList = $('.header-menu ul')
-
-      $(toggleMenu).click(function () {
-        $(menuList).slideToggle()
-      })
-
-    });
-
-    setTimeout(function () {
-      $('body').addClass('page-loaded');
-    }, 5000);
-  }
 
 }
 
